f5378bf3b823943a67a7a99d4ac509172455329c
Jason Gretz <jagretz@gmail.com>
2020-10-03 12:52:56 -0400

    (feat) Add firebase web setup.
    - Add dotenv environment-based configuration for application secrets.
    - Add dotenv to .gitignore

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/greetings/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Application", () => {
+  it("should render without crashing", () => {
+    render(<App />);
+  });
+
+  // it("renders learn react link", () => {
+  //   const { getByText } = render(<App />);
+  //   const linkElement = getByText(/greetings/i);
+  //   expect(linkElement).toBeInTheDocument();
+  // });
 });
--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,9 +1,22 @@
-import React from "react";
+import app from "firebase/app";
 
-export function Firebase() {
-  return (
-    <div>
-      <h1>Firebase</h1>
-    </div>
-  );
+/**
+ * The `REACT_APP_` env-variable prefix is required for apps bootstropped
+ * with create-react-app.
+ */
+const firebaseConfig = Object.freeze({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+export default class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+  }
 }
